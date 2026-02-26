3f66280156d78ceab39eea3220f50d70b43a6532
Ritwik <ritwikvinay38@gmail.com>
2026-02-26 20:25:44 +0530

    init commit

--- src/basics/StringConcat.java
+++ src/basics/StringConcat.java
@@ -1,3 +1,5 @@
+package basics;
+
 public class StringConcat {
 
     static void main() {
--- src/basics/integerPractice.java
+++ src/basics/integerPractice.java
@@ -1,6 +1,11 @@
+package basics;
+
 public class integerPractice {
     static void main() {
         int x=10;
+
+        System.out.println(++x);
+        System.out.println(x++);
         System.out.println(x++ + ++x);
     }
 }
