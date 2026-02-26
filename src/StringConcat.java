eea5b62e9b4a2cc708d950f7c817444b2c2e3a7a
Ritwik <ritwikvinay38@gmail.com>
2026-02-26 19:34:18 +0530

    Initial commit

---
+++ src/StringConcat.java
@@ -0,0 +1,11 @@
+public class StringConcat {
+
+    static void main() {
+        String s1= "Selenium";
+        String s2= s1.concat(" WebDriver");
+        String s3= s1;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1==s3);
+    }
+}
---
+++ src/integerPractice.java
@@ -0,0 +1,6 @@
+public class integerPractice {
+    static void main() {
+        int x=10;
+        System.out.println(x++ + ++x);
+    }
+}
