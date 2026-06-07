5edc6a8001a1bc8c3d797657f4c37ce93dc9badd
Ritwik <ritwikvinay38@gmail.com>
2026-06-07 20:30:47 +0530

    Step 3: Update Build Configuration to Java 25 - pom.xml source/target updated to 25

---
+++ src/main/java/com/practice/basics/TestNewPractice/LeapYear.java
@@ -0,0 +1,13 @@
+package com.practice.basics.TestNewPractice;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the Year== ");
+        int n = scn.nextInt();
+         
+    }
+    
+}
---
+++ src/main/java/com/practice/basics/TestNewPractice/leftAngleTriangle.java
@@ -0,0 +1,5 @@
+package com.practice.basics.TestNewPractice;
+
+public class leftAngleTriangle {
+    
+}
