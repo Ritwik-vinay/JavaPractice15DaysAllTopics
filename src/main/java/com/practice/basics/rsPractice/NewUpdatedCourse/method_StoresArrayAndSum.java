a94c8f4393009f9131e6bfc2f3a487ed6afd32a0
Ritwik Vinay <ritwikvinay@Ritwiks-MacBook-Neo.local>
2026-08-12 10:45:31 +0530

    Step 3: Upgrade Maven compiler settings to Java 21 - Compile: SUCCESS

--- src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/method_StoresArrayAndSum.java
+++ src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/method_StoresArrayAndSum.java
@@ -1,5 +1,16 @@
 package com.practice.basics.rsPractice.NewUpdatedCourse;
 
 public class method_StoresArrayAndSum {
-    
+    public static int sum(int[] arr) {
+        int s = 0;
+        if (arr != null) {
+            for (int v : arr) s += v;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        System.out.println("Sum: " + sum(a));
+    }
 }
