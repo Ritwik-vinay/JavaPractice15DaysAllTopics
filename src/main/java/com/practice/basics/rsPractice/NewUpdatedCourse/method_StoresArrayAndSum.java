2f042da78fa810763bbe05aae4c8d0cf4828ecb2
Ritwik <vinayritwik@gmail.com>
2026-08-12 12:23:25 +0530

    Fix sumOfArray method implementation and correct main method output

--- src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/method_StoresArrayAndSum.java
+++ src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/method_StoresArrayAndSum.java
@@ -2,8 +2,15 @@ package com.practice.basics.rsPractice.NewUpdatedCourse;
 
 public class method_StoresArrayAndSum {
     public static void main(String[] args) {
-        
+        int[] arr= {1,2,3,4,5};
+        System.out.println(sumOfArray(arr););
     }
-    static void sumOfArray(){}
+    static int sumOfArray(int[] arr){
+        int sum=0;
+        for(int i: arr){
+            sum+=i;
+        }
+        return sum;
+    }
 }
     
