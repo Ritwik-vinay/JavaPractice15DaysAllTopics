6eef034cd2688e3276e805c3c3ca70a6ea08d144
Ritwik <vinayritwik@gmail.com>
2026-08-14 09:58:18 +0530

    Step 3: Finalize Java target (set pom.xml target/source to 21) - Compile: SUCCESS, Tests: 1/1 passed

---
+++ src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/multiplication.java
@@ -0,0 +1,17 @@
+package com.practice.basics.rsPractice.NewUpdatedCourse;
+
+public class multiplication {
+       public static void main(String[] args) {
+      multiplicationMethod(5);
+        
+    }
+    static void multiplicationMethod(int number){
+        int result=0;
+        for(int i=0;i<=10;i++){
+            result= result+number;
+            System.out.println(result);
+        }
+       
+    }
+    
+}
