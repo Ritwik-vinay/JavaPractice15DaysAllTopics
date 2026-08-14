377439e9d6bf7aa679123ef6c2b4306b5131e795
Ritwik <vinayritwik@gmail.com>
2026-08-14 16:48:44 +0530

    Add new Java classes for array operations: adjacent index difference, common elements, and sorting in ascending/descending order

---
+++ src/main/java/com/practice/basics/rsPractice/NewUpdatedCourse/commonArrays2.java
@@ -0,0 +1,25 @@
+package com.practice.basics.rsPractice.NewUpdatedCourse;
+
+import java.util.ArrayList;
+public class commonArrays2 {
+    public static void main(String[] args) {
+        int[]a={1,4,8,12,15};
+        int[] b = {1,3,8,12,0};
+        getCommonArray(a, b);
+    }
+    static void getCommonArray(int[] a, int [] b){
+        ArrayList<Integer> al = new ArrayList<>();
+
+        for(int i=0;i<a.length;i++){
+            if (a[i]==b[i]) {
+                al.add(a[i]);
+            }
+        }
+        Object[] ab= al.toArray();
+        for(Object obj :ab){
+            System.out.println(obj);
+        }
+
+    }
+    
+}
